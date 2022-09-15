996f01afe2daef479badc57fc9eafbed8e5fd51c
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-09-15 12:08:23 +0300

    adding game
    Added materials
    Added ground with friction physic
    Added Rigidbody component rb
    Added C# script with player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //This is a referance to the Rigidbody component called "rb"
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 500f;
+
+    //We use FixedUpdates instead of Update because we 
+    //are messing with the physics in Unity
+    void FixedUpdate()
+    {
+        // rb.useGravity = false;
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // x, y, z-axis
+        PlayerMove();
+    }
+
+
+    void PlayerMove()
+    {
+        //if input a or LeftArrow
+        if (Input.GetKey("a") || (Input.GetKey(KeyCode.LeftArrow)))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        //if input d or RightArrow
+        if (Input.GetKey("d") || (Input.GetKey(KeyCode.RightArrow)))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
