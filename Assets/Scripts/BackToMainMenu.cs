dc8f250c023ab2cf2ac36f0bad674b9c42577158
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-09-19 17:23:15 +0300

    main menu and credits
    added main menu
    added credits

---
+++ Assets/Scripts/BackToMainMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,15 @@ public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
     public float restartDelay = 2f;
+
+    // public GameObject completeLevelUI;
+
+    // public void CompleteLevel()
+    // {
+    //     completeLevelUI.SetActive(true);
+    //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    // }
     public void GameOver()
     {
         if (gameHasEnded == false)
